da7af9156e2953a6265ed2ed00bf6a763881b0aa
Sloth <sloth.software.dev@protonmail.com>
2025-07-15 21:00:29 -0700

    feat: implement video download logic into electronAPI

--- src/types/electron-api.d.ts
+++ src/types/electron-api.d.ts
@@ -12,6 +12,13 @@ declare global {
             saveVideoFile: (defaultName?: string) => Promise <FolderPickerResult>;
             downloadVideoPython: (args: T) => Promise <string | null>; 
             getVideoInfoPython: (url: string, options?: T) => Promise <T>
+            openDirectory: (path: string) => Promise<T>
+            openFileInDirectory: (filePath: string) => Promise<T>
         };
+        electronAPIPythonDownload: {
+            startDownloadPython: (options: T) => Promise<T>;
+            onProgressUpdate: (option: T) => Promise<T>;
+            removeProgressListener: () => void;
+        }
     }
 }
